2336c4d3a7d1caaf2e75588f8bbdf0d3376fa568
BearND <bsitzmann@wikimedia.org>
2018-05-30 10:31:19 -0600

    Moved most of the cookie and network interaction to the injected page
    When "keep me logged in" was not used during log in, the session cookie
    is not accessible in the global page. By moving the network request to
    fetch the CSRF token to the injected/content page (popups.js) this
    works now.
    
    So, the structure is more like this:
    * global page: for UI + I18N
    * injected page: network requests + DOM handling of current wiki page
    
    Between those messages have to be dispatched. Care has to be taken that
    only simple, cloneable objects are passed with the message as message
    data.
    
    Bug: T191998

--- WikipediaReadingLists.safariextension/injected.js
+++ WikipediaReadingLists.safariextension/injected.js
@@ -1,5 +1,105 @@
-safari.self.addEventListener('message', (event) => {
-    if (event.name === 'wikiExtensionGetPageTitle') {
-        safari.self.tab.dispatchMessage('wikiExtensionSetPageTitle', document.querySelector('link[rel=canonical]').href);
+let canonicalPageTitle;
+
+function dispatchShow(what, message) {
+    return Promise.resolve(safari.self.tab.dispatchMessage('wikiExtensionAddPageToReadingList:' + what, message));
+}
+
+function readingListPostEntryUrlForOrigin(origin, listId, token) {
+    return `${origin}/api/rest_v1/data/lists/${listId}/entries/?csrf_token=${encodeURIComponent(token)}`;
+}
+
+function csrfFetchUrlForOrigin(origin) {
+    return `${origin}/w/api.php?action=query&format=json&formatversion=2&meta=tokens&type=csrf`;
+}
+
+function getCsrfToken(origin) {
+    return fetch(csrfFetchUrlForOrigin(origin), { credentials: 'same-origin' })
+    .then(res => res.json())
+    .then(res => res.query.tokens.csrftoken);
+}
+
+function parseTitleFromUrl(href) {
+    const url = new URL(href);
+    return url.searchParams.has('title') ? url.searchParams.get('title') : url.pathname.replace('/wiki/', '');
+}
+
+function getCanonicalPageTitle() {
+    return Promise.resolve(canonicalPageTitle);
+}
+
+function addPageToDefaultList(url, listId, token) {
+    canonicalPageTitle = parseTitleFromUrl(document.querySelector('link[rel=canonical]').href);
+    return getCanonicalPageTitle()
+    .then(title => fetch(readingListPostEntryUrlForOrigin(url.origin, listId, token), getAddToListPostOptions(url, title)))
+    .then(res => res.json())
+    .then(res => dispatchShow('showResult', {urlString: url.href, resString: JSON.stringify(res)}));
+}
+
+function getReadingListsUrlForOrigin(origin, next) {
+    let result = `${origin}/api/rest_v1/data/lists/`;
+    if (next) {
+        result = result.concat(`?next=${next}`);
     }
-});
+    return result;
+}
+
+function getDefaultListId(url, next) {
+    return fetch(getReadingListsUrlForOrigin(url.origin, next), { credentials: 'same-origin' })
+    .then(res => {
+        if (res.status < 200 || res.status > 399) {
+            return res.json().then(res => {
+                throw res;
+            });
+        } else {
+            return res.json();
+        }
+    })
+    .then(res => {
+        const defaultList = res.lists.filter(list => list.default)[0];
+        if (defaultList) {
+            return defaultList.id;
+        } else if (res.next) {
+            return getDefaultListId(url, res.next);
+        } else {
+            throw new Error("no default list");
+        }
+    });
+}
+
+function mobileToCanonicalHost(url) {
+    url.hostname = url.hostname.replace(/^m\./, '').replace('.m.', '.');
+    return url;
+}
+
+function getAddToListPostBody(url, title) {
+    return `project=${mobileToCanonicalHost(url).origin}&title=${encodeURIComponent(title)}`;
+}
+
+function getAddToListPostOptions(url, title) {
+    return {
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        credentials: 'same-origin',
+        body: getAddToListPostBody(url, title)
+    }
+}
+
+function handleTokenResult(url, token) {
+    return token === '+\\'
+        ? dispatchShow('showLoginPrompt', {urlString: url.href})
+        : getDefaultListId(url).then(listId => addPageToDefaultList(url, listId, token));
+}
+
+function handleClick(url) {
+    return getCsrfToken(url.origin).then(token => handleTokenResult(url, token));
+}
+
+safari.self.addEventListener('message', (event) => {
+    if (event.name === 'wikiExtensionAddPageToReadingList') {
+        const urlString = event.message;
+        if (urlString) {
+            const url = new URL(urlString);
+            handleClick(url).catch(err => dispatchShow('showError', {urlString: url.href, errString: err.toString()}));
+        }
+    }
+}, false);
