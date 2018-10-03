ab7bc26efa8b57e44b737482e95e390d2d2fbf5d
Michael Holloway <mholloway@wikimedia.org>
2018-10-03 16:25:25 +0500

    Fix bug with button enabled state when using back or forward buttons
    Fixes a bug in which the page action could be inappropriately shown for
    non-Wikimedia pages when navigating back or forward from a Wikimedia page.
    
    This update makes better use of Safari lifecycle events to disable the
    button before every navigation event, and enable it only when the expected
    conditions are met.
    
    Bug: https://phabricator.wikimedia.org/T197825

--- WikipediaReadingLists.safariextension/validate.js
+++ WikipediaReadingLists.safariextension/validate.js
@@ -25,24 +25,27 @@ function setEnabled(target, cond) {
     target.disabled = !cond;
 }
 
+let button;
+
 safari.application.addEventListener('validate', function (event) {
-    if (event.command === 'wikiAddToReadingList') {
-        const button = event.target;
-        button.disabled = true;
-        if (!(safari.application.activeBrowserWindow
-            && safari.application.activeBrowserWindow.activeTab
-            && safari.application.activeBrowserWindow.activeTab.url)) {
-            return;
-        }
-        const url = new URL(safari.application.activeBrowserWindow.activeTab.url);
-        setEnabled(button, isSupportedHost(url.hostname) && isSavablePage(url.pathname, url.searchParams));
-        safari.application.addEventListener('message', function (event) {
-            if (event.name === 'wikiExtensionFoundPageNamespace') {
-                console.log('foo');
-                console.log(event.message);
-                setEnabled(button, supportedNamespaces.includes(event.message.ns));
-            }
-        });
-        safari.application.activeBrowserWindow.activeTab.page.dispatchMessage('wikiExtensionGetPageNamespace');
+    if (event.command === 'wikiAddToReadingList') button = event.target;
+});
+
+safari.application.addEventListener('message', function (event) {
+    if (button && event.name === 'wikiExtensionFoundPageNamespace') {
+        setEnabled(button, supportedNamespaces.includes(event.message.ns));
+    }
+});
+
+safari.application.addEventListener('beforeNavigate', function (event) {
+    if (button) button.disabled = true;
+});
+
+safari.application.addEventListener('navigate', function (event) {
+    const rawUrl = event.target.url;
+    if (!rawUrl) return;
+    const url = new URL(rawUrl);
+    if (isSupportedHost(url.hostname) && isSavablePage(url.pathname, url.searchParams)) {
+        event.target.page.dispatchMessage('wikiExtensionGetPageNamespace');
     }
 });
